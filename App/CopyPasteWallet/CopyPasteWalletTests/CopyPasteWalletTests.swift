1bf35e5c47bfc2934b048406af8d74a2029fa054
Dharamveer Singh <Kanda202@gmail.com>
2026-07-19 20:22:33 +1000

    Initial commit

---
+++ App/CopyPasteWallet/CopyPasteWalletTests/CopyPasteWalletTests.swift
@@ -0,0 +1,19 @@
+//
+//  CopyPasteWalletTests.swift
+//  CopyPasteWalletTests
+//
+//  Created by Dharamveer Kanda on 19/7/2026.
+//
+
+import Testing
+@testable import CopyPasteWallet
+
+struct CopyPasteWalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
